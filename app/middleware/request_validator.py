5ba15ccf0d6d1c31f32f795c861d6593659a5b66
Sven Schürmann <sven.schuermann@kiongroup.com>
2019-05-13 15:05:52 +0200

    Middleware 'RequestValidator' updated.
     - Short-Circuit added if http request has no payload.

--- app/middleware/request_validator.py
+++ app/middleware/request_validator.py
@@ -10,8 +10,8 @@ from app.models.http_error import HttpError
 class RequestValidator(object):
 
     def process_request(self, req, resp):
-        if (req.content_length in (None, 0)) or (req.method == "OPTIONS"):
-            return
+        if req.content_length in (None, 0):
+            resp.complete = True
 
         body = req.stream.read()
         if not body:
@@ -24,8 +24,6 @@ class RequestValidator(object):
             raise HttpError(400, "JSON was incorrect")
 
     def process_resource(self, req, resp, resource, params):
-        if req.method == "OPTIONS":
-            return
         name = [resource.__class__.__name__, stringcase.lowercase(getattr(req, "method", None)), "request schema"]
         schema = getattr(JsonSchemas, stringcase.snakecase(" ".join(name)), None)
         payload = req.context["doc"]
@@ -39,7 +37,7 @@ class RequestValidator(object):
                 raise HttpError(500, "Unable to check the request because the JSON scheme is invalid.")
 
     def process_response(self, req, resp, resource, req_succeeded):
-        if "doc" not in req.context:
+        if "result" not in req.context:
             return
 
         resp.body = json.dumps(req.context["result"])
