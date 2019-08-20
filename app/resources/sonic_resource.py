f73084c326f062b742db8e7493744518249cd4a9
Tim Dollmann <tim.dollmann@kiongroup.com>
2019-08-20 17:57:52 +0200

    Test response

--- app/resources/sonic_resource.py
+++ app/resources/sonic_resource.py
@@ -7,8 +7,4 @@ class SonicResource(object):
     shield_service = ShieldService()
 
     def on_get(self, req, resp):
-        sonic = self.shield_service.get_sonic()
-        echo_time = self.shield_service.get_sonic_echo_time()
-
-        resp.body = '{"message": "Hello world!"}'
-        resp.status = falcon.HTTP_200
+        resp.body = json.dump({'id': 123, 'name': 'work in office'})
