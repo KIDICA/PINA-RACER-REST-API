797340326eb4fef5ffe680e9805d40812df38048
Tim Dollmann <tim.dollmann@kiongroup.com>
2019-08-20 16:19:46 +0200

    Add self definition

--- app/services/shield_service.py
+++ app/services/shield_service.py
@@ -44,12 +44,12 @@ class ShieldService:
         while cnt != 0:
             cnt = cnt - 1
             for i in range(50, 140, 1):
-                self.__write_reg(self.__CMD_SERVO1, num_map(i, 0, 180, 500, 2500))
+                self.__write_reg(self.__CMD_SERVO1, self.num_map(i, 0, 180, 500, 2500))
                 time.sleep(0.005)
             for i in range(140, 50, -1):
-                self.__write_reg(self.__CMD_SERVO1, num_map(i, 0, 180, 500, 2500))
+                self.__write_reg(self.__CMD_SERVO1, self.num_map(i, 0, 180, 500, 2500))
                 time.sleep(0.005)
-        self.__write_reg(self.__CMD_SERVO1, num_map(90, 0, 180, 500, 2500))
+        self.__write_reg(self.__CMD_SERVO1, self.num_map(90, 0, 180, 500, 2500))
 
     def num_map(self, value, from_low, from_high, to_low, to_high):
         return (to_high - to_low) * (value - from_low) / (from_high - from_low) + to_low
