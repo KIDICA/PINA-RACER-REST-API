7e34c68668760cbb40579fc57c4b5ce4c8f91eb7
Tim Dollmann <tim.dollmann@kiongroup.com>
2019-08-28 10:54:47 +0200

    Expose public

--- app/run.py
+++ app/run.py
@@ -50,7 +50,7 @@ async def time(websocket, path):
         await websocket.send(now)
         await asyncio.sleep(random.random() * 3)
 
-start_server = websockets.serve(time, "127.0.0.1", 5678)
+start_server = websockets.serve(time, "0.0.0.0", 5678)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
